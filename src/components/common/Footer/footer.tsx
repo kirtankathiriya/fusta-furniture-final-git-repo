a7edec678f3779912ef1de965d6c348b9e62e70e
Patel_Het <patelhet2264@gmail.com>
2024-01-04 15:45:50 +0530

    useless commit

--- src/components/common/Footer/footer.tsx
+++ src/components/common/Footer/footer.tsx
@@ -4,23 +4,25 @@ import Image from 'next/image';
 import { CiLocationOn } from "react-icons/ci";
 import { MdOutlineLocalPostOffice } from "react-icons/md";
 import { CiPhone } from "react-icons/ci";
-import Location from '../../../../public/icon/location.svg'
+
 type Props = {}
 
 const Footer = (props: Props) => {
     return (
         <>
             <section className="grid grid-cols-1 md:grid-cols-2 bg-slate-100  font-normal">
-                <div className='w-full tracking-widest p-10'>
+                <div className='w-full tracking-widest gap-2 flex flex-col p-10'>
                     <h1 className='pt-6 mb-4 font-bold '>My Account</h1>
-                    <p className='flex hover:text-red-500 cursor-pointer items-center'>
-                        <Location clasName="h-5"/>
+                    <p className='flex hover:text-red-500 cursor-pointer items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                    </svg>
                         184 Main Rd E,St Albans VIC 3021,Australia</p>
-                    <p className=' flex hover:text-red-500 cursor-pointer items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                    <p className=' flex hover:text-red-500 cursor-pointer items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
 </svg>
 Mail Us : yourmail@gmail.com</p>
-                    <p className=' flex hover:text-red-500 cursor-pointer items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                    <p className=' flex hover:text-red-500 cursor-pointer items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
 </svg>
 Phone: + 00 123 456 789 </p>
