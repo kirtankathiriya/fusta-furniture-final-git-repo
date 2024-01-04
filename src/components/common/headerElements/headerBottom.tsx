a232b3178baeaa4ba655e718b577de4287a4b72d
Kirtan_09 <kirtankathiriya09@gmail.com>
2024-01-04 15:36:37 +0530

    quotes are update

--- src/components/common/headerElements/headerBottom.tsx
+++ src/components/common/headerElements/headerBottom.tsx
@@ -58,115 +58,11 @@ export const HeaderBottom = () => {
       <div className="hidden md:block">
         <ul className='flex w-full gap-5 font-normal '>
           <li
-            onMouseEnter={toggleHomeMenu}
-            onMouseLeave={toggleHomeMenu}
             className='relative'
           >
-            <a href='/' className='hover:text-[#E97730]'>Home</a>
-            {/* {isHomeMenuOpen && (
-              <ul className='absolute bg-white p-2 mt-1 w-28'>
-                <li>
-                  <a href='#' className='hover:text-[#E97730]'>Home 1</a>
-                </li>
-                <li>
-                  <a href='#' className='hover:text-[#E97730]'>Home 2</a>
-                </li>
-                <li>
-                  <a href='#' className='hover:text-[#E97730]'>Home 3</a>
-                </li>
-                <li>
-                  <a href="#" className='hover:text-[#E97730]'>Home Rtl</a>
-                </li>
-              </ul>
-            )} */}
+          <a href='/' className='hover:text-[#E97730]'>Home</a>
           </li>
-
-          {/* <li
-            onMouseEnter={menuList}
-            onMouseLeave={menuList}
-            className='relative ml-0'
-          >
-            <a href="#" className='hover:text-[#E97730]'>Pages</a>
-            {
-              isMenu && (
-                <div className='grid grid-cols-3 fixed'>
-                  <div className='relative'>
-                    <ul className='aabsolute bg-white p-3 mt-1 shadow-md'>
-                      <li>
-                        <h3 className='font-bold'>Column Three</h3>
-                      </li>
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Faq</a>
-                      </li >
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Login</a>
-                      </li>
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Register</a>
-                      </li>
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">My Account Page</a>
-                      </li>
-                    </ul>
-                  </div>
-
-                  <div className='relative'>
-                    <ul className='absolute bg-white p-3 mt-1 shadow-md'>
-                      <li>
-                        <h3 className='font-bold'>Column Two</h3>
-                      </li>
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Product Detail</a>
-                      </li >
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Shop Left Sidebar</a>
-                      </li>
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Shop Rigth Sidebar</a>
-                      </li>
-                    </ul>
-                  </div>
-
-                  <div className='relative'>
-                    <ul className='absolute bg-white p-3 mt-1 shadow-md'>
-                      <li>
-                        <h3 className='font-bold'>Column One</h3>
-                      </li>
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Product Detail 2</a>
-                      </li >
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Error 404</a>
-                      </li>
-                      <li className='hover:text-[#E97730]'>
-                        <a href="#">Curt Page</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              )
-            }
-          </li> */}
-
-
-          {/* <li
-            onMouseEnter={toggleShopMenu}
-            onMouseLeave={toggleShopMenu}
-            className='relative ml-3'
-          >
-            <a href='/Products' className='hover:text-[#E97730]'>Shop</a>
-            {isShopMenuOpen && (
-              <ul className='absolute bg-white p-3 mt-1 shadow-md w-28'>
-                <li>
-                  <a href='#' className='hover:text-[#E97730]'>Product 1</a>
-                </li>
-                <li>
-                  <a href='#' className='hover:text-[#E97730]'>Product 2</a>
-                </li>
-              </ul>
-            )}
-          </li> */}
-
+          
           <li className='relative ml-3'>
             <a href="/Products" className='hover:text-[#E97730]'>Product</a>
           </li>
@@ -183,31 +79,27 @@ export const HeaderBottom = () => {
 
       {/* Right section with icons */}
       <div className='flex items-center'>
-        <div className='flex gap-4 mr-0'>
+        <div className='flex gap-2 mr-0'>
           <a href='#' className='hover:text-[#E97730]'>
-            {/* <FaSearch className='w-7 h-7' /> */}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
 
           </a>
           <a href='/Account/Loginpage' className='hover:text-[#E97730]'>
             < SlUser className='w-6 h-6' />
-
-
           </a>
           <a href='Checkout' className='hover:text-[#E97730]'>
-            {/* <IoCart className='w-7 h-7' /> */}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-[26px] h-[26px]">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
             </svg>
 
           </a>
         </div>
-        <div className='md:hidden'>
-          <ImMenu3 className='w-7 h-7' onClick={toggleMobileMenu} />
+        <div className='md:hidden mx-2'>
+          <ImMenu3 className='w-6 h-6' onClick={toggleMobileMenu} />
           {isMobileMenuOpen && (
-            <section className='absolute left-0 w-full text-orange-500 border-gray-200 mt-6 p-2 ml-2 '>
+            <section className='absolute left-0 w-full text-orange-500 border-gray-200 mt-6 p-2 ml-2 bg-'>
               <ul>
                 <li>Home</li>
                 <li>Pages</li>
--- src/containers/Quotes/Quotes.tsx
+++ src/containers/Quotes/Quotes.tsx
@@ -11,17 +11,17 @@ const Quotes: React.FC = () => {
     },
     {
       id: 2,
-      text: "Quos temporibus quis ad itaque voluptatibus nulla magnam perferendis reiciendis cupiditate culpa earum at, odio sed aut suscipit?",
+      text: "Quos temporibus quis ad itaque voluptatibus nulla magnam perferendis",
       author: "Author 2",
     },
     {
       id: 3,
-      text: "Quos temporibus quis ad itaque voluptatibus  cupiditate dolorum nostrum consequatur culpa earum at, odio sed aut suscipit?",
+      text: "Quos temporibus quis ad itaque voluptatibus  cupiditate dolorum",
       author: "Author 3",
     },
     {
       id: 4,
-      text: "Quos temporibus quis ad itaque  nostrum consequatur culpa earum at, odio sed aut suscipit?",
+      text: "Quos temporibus quis ad itaque  nostrum consequatur",
       author: "Author 4",
     },
   ];
@@ -41,27 +41,30 @@ const Quotes: React.FC = () => {
   }, []);
 
   return (
-    <div className="relative ">
+    <div className="relative bg-black">
       {/* <img
         src="https://fusta-demo.myshopify.com/cdn/shop/files/02.jpg?v=1613745605"
         alt="Your Descriptive Alt Text"
         className=" opacity-50 w-full h-96 md:h-full min-"
         
       /> */}
-        <div
-          className=" justify-center top-8 bottom-0 left-0 right-0  min-w-full text-center p-24 bg-no-repeat bg-scroll bg-center bg-cover backdrop-filter backdrop-blur-smop-blur-sm my-5"
-          style={{ backgroundImage: "url(/Quotes.jpg)" }}
-        >
+      <div
+        className=" justify-center top-8 bottom-0 left-0 right-0  text-center p-2 md:p-24 bg-no-repeat bg-scroll bg-center bg-cover opacity-70 backdrop-filter backdrop-blur-smop-blur-sm my-5 "
+        style={{ backgroundImage: "url(/Quotes.jpg)" }}
+      >
+        <div>
           <p className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-white font-serif">
             ❝
           </p>
-          <p className="whitespace-normal font-semibold text-2x1  text-white">
+          <p className="whitespace-normal font-semibold md:text-2x1  text-white">
             {slides[currentSlide].text}
           </p>
           <p className="flex justify-center py-3 text-sm  text-white ">
             {slides[currentSlide].author}
           </p>
         </div>
+
+      </div>
     </div>
   );
 };
